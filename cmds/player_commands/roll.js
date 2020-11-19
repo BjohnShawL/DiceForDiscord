a0e7341b54800fb17f5906ae79b7946974e5ee7f
BJohnShawL <bjohnshawl@gmail.com>
2020-11-19 16:35:46 +0000

    Added embed message on roll

--- cmds/player_commands/roll.js
+++ cmds/player_commands/roll.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const Commando = require('discord.js-commando');
 const axios = require('axios');
 
@@ -13,49 +14,67 @@ module.exports = class Roll extends Commando.Command {
 
     }
 
-    
+
 
     run = async (message, args) => {
 
-        function createUrl(argumentArray)  {
-            if(argumentArray.length === 1){
+        function createUrl(argumentArray) {
+            if (argumentArray.length === 1) {
                 return 'http://roll.diceapi.com/json/' + argumentArray[0].toString()
             }
-            else{
+            else {
                 const arr = argumentArray.toString().split(" ");
-            let argument = "http://roll.diceapi.com/json/"
-            for (let i = 0; i < arr.length; i++) {
-                const x = arr[i];
-                argument+=(x+"/");
-                
+                let argument = "http://roll.diceapi.com/json/"
+                for (let i = 0; i < arr.length; i++) {
+                    const x = arr[i];
+                    argument += (x + "/");
+
+                }
+                return argument;
             }
-            return argument;
-        }}
+        }
 
         if (args.length < 1) { message.author.send(`Please send a message like '!roll 3d6', with the number and type of dice in the format <number>d<sides_of_dice>. Thanks.`) }
 
         const { guild, channel } = message;
-        const member = message.member;
+        const member = message.member.user;
 
         if (channel.type != 'dm') {
 
             const diceUrl = createUrl(args)
 
-            console.log(diceUrl);
 
             axios.get(diceUrl)
                 .then((res) => {
                     const result = res.data;
-
-                    let r = "Result: ";
+                    
+                    let r = "";
                     for (let i = 0; i < result.dice.length; i++) {
                         r += result.dice[i].value.toString();
                         r += ', ';
                     }
+                    ;
+                    let resultArray = r.split(", ");
+                    const resArray = resultArray.splice(0, resultArray.length - 1);
+                    const batches = [];
+                    for (let i = 0; i < args.toString().split(" ").length; i++) {
+                        var numOfDice = args.toString().split(" ")[i];
+                        let batchsize = parseInt(numOfDice);
+                        batches.push(batchsize)
 
-                    //TODO: Create embedded message which seperates the batches of dice rolled (if multiple batches are rolled)
+                    }
+                    
+                    const e_name = message.member.nickname || member.username
+                    const embed = new MessageEmbed().setAuthor(`--${e_name} has rolled the dice--`).setTitle('Results :').setColor('#4E6F7B');
 
-                    message.author.send(r)
+                    for (let i = 0; i < batches.length; i++) {
+                        const element = { name: `roll # ${i + 1}`, value: resArray.splice(0, batches[i]).join(', ') };
+
+                        embed.addField(element.name, element.value);
+                    }
+
+                    channel.send(embed);
+
                 }).catch((err) => {
                     console.error('ERROR: ', err)
                 })
@@ -64,7 +83,6 @@ module.exports = class Roll extends Commando.Command {
         else {
             message.author.send(`You need to send this command from the in-game channel`);
 
-            // console.log(message.client.guilds.cache)
 
         }
 
@@ -72,5 +90,5 @@ module.exports = class Roll extends Commando.Command {
 
 
     }
-    
+
 }
